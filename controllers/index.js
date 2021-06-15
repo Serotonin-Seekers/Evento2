4e9b9e0adb111060ea12b2ee15b7a056e78631f2
heganjr <jacobhegan@hotmail.com>
2021-06-15 21:24:59 +0800

    addevent.html route added

--- controllers/index.js
+++ controllers/index.js
@@ -5,4 +5,8 @@ router.get("/", async (request, response) => {
   response.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
+router.get("/addevent.html", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../views/addevent.html"));
+});
+
 module.exports = router;
